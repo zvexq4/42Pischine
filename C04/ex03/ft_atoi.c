d1035868b82c572a241bd7fdb55f1edc251aeea8
Hamza Çiftçi <ghost@ghost.local>
2026-05-10 20:54:40 +0300

    C04 ex03' e kadar tamamladı

---
+++ C04/ex03/ft_atoi.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hciftci <hciftci@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/10 19:51:32 by hciftci           #+#    #+#             */
+/*   Updated: 2026/05/10 20:24:14 by hciftci          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		ft_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	int		result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *=  -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+int main(void)
+{
+	printf("%d\n", ft_atoi("   ---+--+12345abc"));
+}
